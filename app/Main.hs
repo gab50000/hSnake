d2e293114d236018ad97a9d9c0a6069034241876
Gabriel Kabbe <gabriel.kabbe@mail.de>
2022-04-01 19:54:55 +0200

    Add widget with 2d grid

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,13 @@
 module Main where
 
 import Brick
+import Brick.Widgets.Border
 
 ui :: Widget ()
-ui = str "Hello, world!"
+ui = vBox [hBox [str "X" | i <- [1..width]] | i <- [1..height]] 
+    where
+        height = 15
+        width = 30
 
 main :: IO ()
 main = simpleMain ui

9e3ea4134356112c28cce28c6d74884985e68215
Gabriel Kabbe <gabriel.kabbe@mail.de>
2022-04-02 11:55:42 +0200

    Rename transform -> handleEvent

--- app/Main2.hs
+++ app/Main2.hs
@@ -9,15 +9,15 @@ import Control.Monad (forever, void)
 
 data Tick = Tick
 
-data AppState n = AppState {text :: String}
+newtype AppState n = AppState {text :: String}
 
 
-transform :: AppState () -> BrickEvent () e -> EventM () (Next (AppState ()))
-transform p (VtyEvent e) = case e of
+handleEvent :: AppState () -> BrickEvent () e -> EventM () (Next (AppState ()))
+handleEvent p (VtyEvent e) = case e of
     V.EvKey (V.KChar 'q') [] -> halt p 
     V.EvKey (V.KChar char) [] -> continue $ p { text = [char]}
     _ -> continue p
-transform p _ = continue p
+handleEvent p _ = continue p
 
 
 drawUI :: AppState () -> [Widget ()]
@@ -28,7 +28,7 @@ initApp :: App (AppState ()) e ()
 initApp = App {
     appDraw = drawUI,
     appChooseCursor = showFirstCursor,
-    appHandleEvent = transform,
+    appHandleEvent = handleEvent,
     appStartEvent = return,
     appAttrMap = const $ attrMap V.defAttr []
 }
